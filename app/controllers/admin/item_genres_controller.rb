2ffb496c549bf4e94d1ab45373b0cbe1d47a2066
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-06-18 23:13:47 +0000

    fixed devese

---
+++ app/controllers/admin/item_genres_controller.rb
@@ -0,0 +1,2 @@
+class Admin::ItemGenresController < ApplicationController
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create(email: "1@1", encrypted_password: "123456")
+Customer.create
