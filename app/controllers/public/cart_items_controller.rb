af1da77ff51fb4d40792ff322ef335df45a12ee4
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-06-23 14:57:38 +0000

    [update]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,5 +6,24 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: @cart_item.item_id)
+      cart_item = current_customer.cart_items.find_by(item_id: @cart_item.item_id)
+      cart_item.count += @cart_item.count.to_i
+      cart_item.save
+      redirect_to cart_items_path
+    else
+      @cart_item.save
+      redirect_to cart_items_path
+    end
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:customer_id,
+                                      :item_id,
+                                      :count
+                                      )
   end
 end
