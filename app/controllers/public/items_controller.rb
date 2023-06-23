c61f75a10674cfa2d05dd38c3fe85d57c3b8ab14
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-06-23 00:03:34 +0000

    [add]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,12 @@
 class Public::ItemsController < ApplicationController
+
   def index
+    @items = Item.all
+    @genres = ItemGenre.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    @genres = ItemGenre.all
   end
 end
