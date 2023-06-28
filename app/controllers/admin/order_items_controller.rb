dbcafd7acd8236af59f582d2914f6a3accc39526
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-06-28 01:43:33 +0000

    [add]status

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -3,8 +3,19 @@ class Admin::OrderItemsController < ApplicationController
   def update
     order_item = OrderItem.find(params[:id])
     order      = order_item.order
-    order_item.update(order_item_params)
-    redirect_to admin_order_path(order)
+    OrderItem.transaction do
+      order_item.update(order_item_params)
+      if order_item.status == "making"
+        order.update(status: 2)
+      elsif order.order_items.count == order.order_items.where(status: "complete").count
+        order.update(status: 3)
+      end
+    end
+      flash[:notice] = "updated order status successfully"
+      redirect_to admin_order_path(order)
+    rescue => e
+      flash[:alert] = "failed update"
+      redirect_to admin_order_path(order)
   end
 
   private
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,14 +3,23 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @total = @order.order_items.inject(0) { |sum, order_item| sum + order_item.total_price }
-    
+
   end
 
   def update
-    @order      = Order.find(params[:id])
-    @order_items = OrderItem.where(order_id: params[:id])
-    @order.update(order_params)
-    redirect_to admin_order_path(@order)
+    order       = Order.find(params[:id])
+    order_items = OrderItem.where(order_id: params[:id])
+    Order.transaction do
+      order.update(order_params)
+      if order.status == "check"
+        order_items.update(status: 1)
+      end
+    end
+      flash[:notice] = "updated order status successfully"
+      redirect_to admin_order_path(order)
+    rescue => e
+      flash[:alert] = "failed update"
+      redirect_to admin_order_path(order)
   end
 
   private
